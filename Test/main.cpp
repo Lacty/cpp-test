210618473697cf84ab9e0d3350b98b1b9cdf81d1
Lacty <akira206@gmail.com>
2015-12-01 14:48:35 +0900

    threadテスト

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,11 +1,26 @@
 
 #include <iostream>
+#include <thread>
 
+
+void task1() {
+  std::cout << "->task1" << std::endl;
+  std::cout << "<-task1" << std::endl;
+}
+
+void task2() {
+  std::string str;
+  std::cout << "->task2" << std::endl;
+  std::cin >> str;
+  std::cout << str << std::endl;
+  std::cout << "<-task2" << std::endl;
+}
 
 int main()
 {
-  std::string line;
-  std::getline(std::cin, line);
-  std::cout << line << std::endl;
+  std::thread th1(&task1);
+  std::thread th2(&task2);
+  th2.join();
+  th1.join();
   return 0;
 }
